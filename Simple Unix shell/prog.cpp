f03c10c7dbee6776f4f940264d7968076d93990b
Diaz-jorge939 <90347792+Diaz-jorge939@users.noreply.github.com>
2023-03-23 13:54:08 -0700

    Update prog.cpp

--- Simple Unix shell/prog.cpp
+++ Simple Unix shell/prog.cpp
@@ -5,8 +5,6 @@
  * @brief This is the main function of a simple UNIX Shell. You may add additional functions in this file for your implementation
  * @version 0.1
  */
-// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
-// Remember to add sufficient and clear comments to your code
 
 #include <stdio.h>
 #include <unistd.h>
